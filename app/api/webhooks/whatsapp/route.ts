cf201e585e46b27e2a00a399ec6ddad3c3df8726
Zaheer Abbas <zaheerabbas@Zaheers-MacBook-Air.local>
2026-06-20 15:22:38 +0530

    Whatsapp token

--- app/api/webhooks/whatsapp/route.ts
+++ app/api/webhooks/whatsapp/route.ts
@@ -3,10 +3,38 @@ import { NextRequest } from "next/server";
 export async function GET(
   request: NextRequest
 ) {
-  return Response.json({
-    verifyToken:
-      process.env.WHATSAPP_VERIFY_TOKEN ?? "NULL",
-  });
+  const searchParams =
+    request.nextUrl.searchParams;
+
+  const mode =
+    searchParams.get("hub.mode");
+
+  const token =
+    searchParams.get("hub.verify_token");
+
+  const challenge =
+    searchParams.get("hub.challenge");
+
+  console.log(
+    "WHATSAPP VERIFY:",
+    {
+      mode,
+      token,
+      challenge,
+    }
+  );
+
+  if (
+    mode === "subscribe" &&
+    token === process.env.WHATSAPP_VERIFY_TOKEN
+  ) {
+    return new Response(challenge);
+  }
+
+  return new Response(
+    "Forbidden",
+    { status: 403 }
+  );
 }
 
 export async function POST(
