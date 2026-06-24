024f97f763e641b0bced2f456c56f4f0b7674202
goswami-rohit <rohit.second44@gmail.com>
2026-06-24 15:09:31 +0530

    looks good3

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 export default function Footer() {
+  const pathname = usePathname();
+  
+  // This will be true ONLY on the wealth page
+  const isWealthPage = pathname === "/wealth";
+
   return (
-    <footer className="border-t py-10">
+    <footer className="border-t py-10 relative z-10 bg-background">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6">
 
         <div>
@@ -18,11 +27,14 @@ export default function Footer() {
             © 2026 Niveshya
           </p>
           
-          <img 
-            src="/footer.png" 
-            alt="Footer Logo" 
-            className="h-14 w-auto object-contain drop-shadow-md transition-transform duration-300 hover:scale-105"
-          />
+          {/* Only render the image if we are on the wealth page */}
+          {isWealthPage && (
+            <img 
+              src="/footer.png" 
+              alt="Partner Logos" 
+              className="h-14 w-auto object-contain drop-shadow-md transition-transform duration-300 hover:scale-105"
+            />
+          )}
         </div>
 
       </div>
